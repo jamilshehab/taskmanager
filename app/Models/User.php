2b88c6d4deacf6de060214a873922293a85fce44
jamilshehab <shehabjamil20@gmail.com>
2025-08-17 22:56:59 -0700

    Changes Added

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -38,6 +40,7 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+ 
     protected function casts(): array
     {
         return [
@@ -45,4 +48,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function tasks(){
+      return $this->hasMany(Task::class);
+    }
+
 }
